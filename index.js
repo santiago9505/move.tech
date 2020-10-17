4e039a7f53842a33ebdcd0a6107108815e55bced
Santiago Guáqueta <zagroller@hotmail.com>
2020-10-17 17:50:38 -0500

    Cuadro el footer y anexo botones de pago

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function enlace()
 {
-  location.href = "https://forms.office.com/Pages/ResponsePage.aspx?id=gcPCyy4vk02R0VBskxas5_uaYzbSud1LmU6-S958qnFUMVZEQ0paT0JLVzFGOE5JNEJYVzNEWDQyUS4u"
+  location.href = "https://forms.gle/1idEkpRjWUEfn3Ad9"
+}
+function linkMercadopago()
+{
+  location.href = "https://mpago.li/1YHhctH"
 }
